cf17420bffa90fba680ab6f5b38d0c3b3ce01da5
lai2-24 <lai2-24@itismeucci.com>
2024-11-06 10:41:07 +0100

    2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Server partito");
+        ServerSocket s1= new ServerSocket(3000);
+        ArrayList user = new ArrayList<String>();
+
+        do{
+
+            Socket s= s1.accept();
+            MioThread t= new MioThread(s, user);
+            t.start();
+        }while(true);
     }
 }
--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -1,5 +1,77 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MioThread extends Thread{
-    
+    Socket s;
+    ArrayList user;
+
+    public MioThread(Socket s, ArrayList user){
+        this.s=s;
+        this.user=user;
+    }
+
+    public void run(){
+        try {
+            BufferedReader in= new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            
+            String scelta;
+            String username;
+            String messaggio;
+            String nome;
+
+            
+            nome=in.readLine();
+            
+            if(!user.contains(nome)){
+                user.add(nome);
+            }
+
+            scelta= in.readLine();
+
+            do{
+                scelta=in.readLine();
+                if(scelta.equals("S")){
+                    messaggio="s";
+                    out.writeBytes(messaggio+"\n");
+
+                    username=in.readLine();
+
+                
+                }
+                else if(scelta.equals("L")){
+                    messaggio="l";
+                    out.writeBytes(messaggio+"\n");
+                }
+                else if(scelta.equals("EXIT")){
+                    messaggio="ex";
+                    out.writeBytes(messaggio+"\n");
+                }
+                else{
+                    messaggio="e";
+                    out.writeBytes(messaggio+"\n");
+                }
+
+
+
+
+
+
+
+                    
+            }while(!scelta.equals("ex"));
+            
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
